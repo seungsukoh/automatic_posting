9227be7d7d7c0531a854f9118aa6c9b387e20e27
seanoh <seanoh@mathworks.com>
2026-06-21 01:59:47 +0900

    Scope Pages function routing to API

--- functions/api/[[path]].ts
+++ functions/api/[[path]].ts
@@ -1,5 +1,5 @@
-import worker from "../cloudflare/src/index";
-import type { Env } from "../cloudflare/src/types";
+import worker from "../../cloudflare/src/index";
+import type { Env } from "../../cloudflare/src/types";
 
 export const onRequest: PagesFunction = async (context) => {
   return worker.fetch(context.request, context.env as unknown as Env);
